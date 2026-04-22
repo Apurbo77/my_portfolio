ebec58f405b3d7a57b1c8b7babe27f2a9e19593b
Apurbo Bhaket <161101753+Apurbo77@users.noreply.github.com>
2026-04-22 23:19:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ if (contactForm) {
 
         const formData = new FormData(this);
 
-        fetch("https://formsubmit.co/ajax/apurbobhaket@gmail.com", {
+        fetch("https://formsubmit.co/ajax/apurbobhaket17@gmail.com", {
             method: "POST",
             body: formData,
             headers: {
